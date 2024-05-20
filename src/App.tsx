9e572d5f5c178db194800a4174db1ff05a420431
Yrjo47 <arhegor@gmail.com>
2024-05-20 21:16:08 +0300

    Basic set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./App.css"
 import { Routes, Route } from "react-router-dom"
 
 const App: React.FC = () => {
-    return <Routes></Routes>
+    return (
+        <Routes>
+            <Route path="/">
+                <Route index />
+                <Route path=":postId" />
+            </Route>
+            <Route path="/*" />
+        </Routes>
+    )
 }
 
 export default App
